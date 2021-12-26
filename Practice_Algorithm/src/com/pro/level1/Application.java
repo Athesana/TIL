ddb50158916dd6bb51fdc93e3e88c55333f6f59b
Athesana <ssskdsal@gmail.com>
2021-12-26 18:42:16 +0900

    20211226 휴대폰번호 뒷자리 제외 * 찍기

--- Practice_Algorithm/src/com/pro/level1/Application.java
+++ Practice_Algorithm/src/com/pro/level1/Application.java
@@ -1,11 +1,20 @@
 package com.pro.level1;
 
+import java.util.Scanner;
+
 public class Application {
 
 	public static void main(String[] args) {
 		
-		new Solution().method1();
-
+//		new Solution().method1();
+		Solution solution = new Solution();
+		Scanner scanner = new Scanner(System.in);
+		String phone_number = scanner.next();
+		System.out.println(solution.solution(phone_number));
+		
+		
+		
 	}
+
 
 }
--- Practice_Algorithm/src/com/pro/level1/Solution.java
+++ Practice_Algorithm/src/com/pro/level1/Solution.java
@@ -4,19 +4,28 @@ import java.util.Scanner;
 
 public class Solution {
 	public void method1() {
-		 Scanner sc = new Scanner(System.in);
-	        int a = sc.nextInt();
-	        int b = sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
 
-	        
-	        
-	        for(int i=0; i < b; i++){
-	            for(int j=0; j < a; j++){
-	                System.out.print("*");
-	            }
-	            System.out.println();
-	        }
+		for (int i = 0; i < b; i++) {
+			for (int j = 0; j < a; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 
 	}
-	
+
+	public String solution(String phone_number) {
+		String answer = "";
+
+		for (int i = 0; i < phone_number.length() - 4; i++)
+			answer += "*";
+
+		answer += phone_number.substring(phone_number.length() - 4);
+
+		return answer;
+	}
+
 }
