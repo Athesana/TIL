e6b2e8b9b9940b6317a88eb45c65cea6f52de5fd
Athesana <ssskdsal@gmail.com>
2022-01-08 21:30:38 +0900

    20220108 배열 평균 구하기

--- Practice_Algorithm/src/com/pro/level1/Application.java
+++ Practice_Algorithm/src/com/pro/level1/Application.java
@@ -7,13 +7,22 @@ public class Application {
 	public static void main(String[] args) {
 		
 //		new Solution().method1();
+		
+		// phone_number solution()
+//		Solution solution = new Solution();
+//		Scanner scanner = new Scanner(System.in);
+//		String phone_number = scanner.next();
+//		System.out.println(solution.solution(phone_number));
+		
+		// int[] arr solution()
 		Solution solution = new Solution();
-		Scanner scanner = new Scanner(System.in);
-		String phone_number = scanner.next();
-		System.out.println(solution.solution(phone_number));
+		
+		int[] arr = {1, 2, 3, 4};
+		System.out.println(solution.solution(arr));
 		
 		
-		
+	
+	
 	}
 
 
--- Practice_Algorithm/src/com/pro/level1/Solution.java
+++ Practice_Algorithm/src/com/pro/level1/Solution.java
@@ -27,5 +27,15 @@ public class Solution {
 
 		return answer;
 	}
+	
+	public double solution(int[] arr) {
+		double answer = 0;
+		
+		for(int i = 0; i < arr.length; i++) {
+			answer += arr[i];
+		}
+		
+		return answer / arr.length;
+	}
 
 }
