5ac34740a491baf8c870c6ed0b3fd4e58c08f082
Athesana <ssskdsal@gmail.com>
2022-02-01 22:12:49 +0900

    20220201 문자열을 정수로 바꾸기

---
+++ Practice_Algorithm/src/com/pro/level1/Solution12925.java
@@ -0,0 +1,20 @@
+package com.pro.level1;
+
+public class Solution12925 {
+
+	public static void main(String[] args) {
+		
+		Solution12925 solution = new Solution12925();
+		System.out.println("-1234");
+		
+	}
+	
+	public int solution(String s) {
+		int answer = 0;
+		
+		answer = Integer.valueOf(s);
+		
+		return answer;
+	}
+
+}
