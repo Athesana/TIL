c253db0a7eca94218a410b428a1da88b5f615564
Athesana <ssskdsal@gmail.com>
2022-02-02 22:34:05 +0900

    20220202 수박수박수

---
+++ Practice_Algorithm/src/com/pro/level1/Solution12922.java
@@ -0,0 +1,27 @@
+package com.pro.level1;
+
+public class Solution12922 {
+
+	public static void main(String[] args) {
+		Solution12922 solution = new Solution12922();
+		
+		System.out.println(solution.solution(5));
+
+	}
+	
+	public String solution(int n) {
+		String answer = "";
+	
+		for(int i = 1; i <= n; i++) {
+			if(i % 2 == 0) {
+				answer += "박";
+			} else {
+				answer += "수";
+			}
+		}
+		
+		return answer;
+		
+	}
+
+}
