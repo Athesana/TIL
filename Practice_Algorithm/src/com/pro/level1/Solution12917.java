4c721c3882423395075de2cc89b1adae5903d9a1
Athesana <ssskdsal@gmail.com>
2022-02-09 19:33:08 +0900

    20220209 문자열 내림차순 배치

--- Practice_Algorithm/src/com/pro/level1/Solution12917.java
+++ Practice_Algorithm/src/com/pro/level1/Solution12917.java
@@ -1,16 +1,28 @@
 package com.pro.level1;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Solution12917 {
 
 	public static void main(String[] args) {
 		
 		Solution12917 solution = new Solution12917();
-		System.out.println();
+		System.out.println(solution.solution("abcdef"));
 
 	}
 	
 	public String solution(String s) {
 		String answer = "";
+		
+		String[] str = s.split("");
+		
+		Arrays.sort(str);
+		
+		Collections.reverse(Arrays.asList(str));
+		
+		answer = String.join("", str);
+		
 		return answer;
 	}
 
