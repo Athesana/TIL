88419e36d8c0de8693c4d44f0665f3b4d5c6f15b
Athesana <ssskdsal@gmail.com>
2022-02-11 20:56:36 +0900

    20220211 두 숫자 사이의 합

---
+++ Practice_Algorithm/src/com/pro/level1/Solution12912.java
@@ -0,0 +1,17 @@
+package com.pro.level1;
+
+public class Solution12912 {
+
+	public static void main(String[] args) {
+		
+		Solution12912 solution = new Solution12912();
+		System.out.println();
+
+	}
+	
+	public long solution(int a, int b) {
+		long answer = 0;
+		return answer;
+	}
+
+}
