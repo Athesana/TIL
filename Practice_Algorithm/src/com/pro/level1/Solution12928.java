d19b06ecbd568572fa87dfa87e12f87617ef80c8
Athesana <ssskdsal@gmail.com>
2022-02-18 13:57:37 +0900

    20220218 약수의 합

--- Practice_Algorithm/src/com/pro/level1/Solution12928.java
+++ Practice_Algorithm/src/com/pro/level1/Solution12928.java
@@ -5,12 +5,19 @@ public class Solution12928 {
 	public static void main(String[] args) {
 		
 		Solution12928 solution = new Solution12928();
-		System.out.println();
+		System.out.println(solution.solution(12));
 		
 	}
 	
 	public int solution(int n) {
 		int answer = 0;
+		
+		for(int i = 1; i <= n; i++) {
+			if(n % i == 0) {
+				answer += i;
+			}
+		}
+		
 		return answer;
 	}
 
