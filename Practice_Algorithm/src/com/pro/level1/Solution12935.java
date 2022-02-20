d940fd8d47626a8333e8d12c5a035e5a50ef7f27
Athesana <ssskdsal@gmail.com>
2022-02-20 19:23:18 +0900

    20220220 작은 수 제거하기 (미완)

---
+++ Practice_Algorithm/src/com/pro/level1/Solution12935.java
@@ -0,0 +1,22 @@
+package com.pro.level1;
+
+import java.util.Arrays;
+
+public class Solution12935 {
+
+	public static void main(String[] args) {
+		
+		Solution12935 solution = new Solution12935();
+		
+		int[] arr = {4, 3, 2, 1};
+		System.out.println(solution.solution(arr));
+		
+	}
+	
+	public int[] solution(int[] arr) {
+		int[] answer = {};
+		
+		return answer;
+	}
+
+}
