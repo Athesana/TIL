736a4ef0d6e09244c91bd111813d1064037402fa
Athesana <ssskdsal@gmail.com>
2022-02-22 21:32:14 +0900

    20220222 같은 숫자는 싫어(미완)

---
+++ Practice_Algorithm/src/com/pro/level1/Solution12906.java
@@ -0,0 +1,20 @@
+package com.pro.level1;
+
+import java.util.Arrays;
+
+public class Solution12906 {
+
+	public static void main(String[] args) {
+		
+		Solution12906 solution = new Solution12906();
+		
+		System.out.println();
+	}
+	
+	public int[] solution(int[] arr) {
+		int[] answer = {};
+		
+		return answer;
+	}
+
+}
