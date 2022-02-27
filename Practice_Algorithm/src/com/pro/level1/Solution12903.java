a33eb7f365a7753657d96b24752f48aa2fc4478e
Athesana <ssskdsal@gmail.com>
2022-02-27 23:23:47 +0900

    20220227 가운데 글자 가져오기(미완)

---
+++ Practice_Algorithm/src/com/pro/level1/Solution12903.java
@@ -0,0 +1,27 @@
+package com.pro.level1;
+
+public class Solution12903 {
+
+	public static void main(String[] args) {
+		
+		Solution12903 solution = new Solution12903();
+		System.out.println(solution.solution("abcde"));
+		
+	}
+	
+	public String solution(String s) {
+		String answer = "";
+		
+		int half = s.length() / 2;
+		
+		if(s.length() % 2 == 0) {
+			
+		} else {
+			
+		}
+		
+		
+		return answer;
+	}
+
+}
