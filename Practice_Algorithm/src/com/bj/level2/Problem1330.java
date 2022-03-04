8f735cfa63f1a0d1961685eade25bf8541e9adc3
Athesana <ssskdsal@gmail.com>
2022-03-04 15:45:31 +0900

    20220304 백준 1330

---
+++ Practice_Algorithm/src/com/bj/level2/Problem1330.java
@@ -0,0 +1,16 @@
+package com.bj.level2;
+
+public class Problem1330 {
+
+	public static void main(String[] args) {
+		
+		int A = 2;
+		int B = 10;
+		
+		if(A > B) System.out.println(">");
+		else if(A < B) System.out.println("<");
+		else System.out.println("==");
+
+	}
+
+}
